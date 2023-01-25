a6783fba10f3e66a0c945c7d0a7fe13f6170dcc7
nic-brian <86538642+nic-brian@users.noreply.github.com>
2023-01-24 23:51:01 -0800

    added week 02 and 03 demos

---
+++ docs/week-03/app.js
@@ -0,0 +1,28 @@
+"use strict";
+
+let count = 0;
+let items = [];
+
+function addItem() {
+    let durationElement = document.getElementById( 'duration' );
+    let duration = durationElement.value;
+    console.log( duration );
+
+    let descriptionElement = document.getElementById( 'description' );
+    let description = descriptionElement.value;
+    console.log( description );
+
+    count += 1;
+
+    items.push( `<tr><th>${count}</th><td>${duration}</td><td>${description}</td></tr>` );
+
+    let todosElement = document.getElementById( 'todos' );
+    todosElement.innerHTML = items.join( '' );
+}
+
+function clearList() {
+    count = 0;
+    items = [];
+    let todosElement = document.getElementById( 'todos' );
+    todosElement.innerHTML = '';
+}
